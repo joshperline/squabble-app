20399bc479450a021bbf6aa1665b91348e66542d
Josh Perline <joshperline@yahoo.com>
2014-03-01 14:07:12 -0800

    Updated Handlers AGAIN

--- main.py
+++ main.py
@@ -43,6 +43,13 @@ class Argument(db.Model):
     #improve to hotness algorithm = db.integer
 
     created = db.DateTimeProperty(auto_now_add = True)
+    #TODO: Update this shitKHJK
+    def score_up1():
+        score += 1
+    def score_up2():
+        score -= 1
+    def ratingUp():
+        rating += 1
 
 class MainHandler(Handler):
     def get(self):
@@ -50,7 +57,7 @@ class MainHandler(Handler):
 
 class NewArgHandler(Handler):
     def get(self):
-        self.render("newArgument.html")
+        self.render("argument.html")
 
     def post(self):
         #TODO: do handling on creating a new argument
@@ -66,33 +73,45 @@ class NewArgHandler(Handler):
             self.redirect("/thanks")#TODO: thanks.html
         else:
             error = "Please fill in all required information."
-            self.render("newArgument.html", title = title, name1 = name1,
+            self.render("argument.html", title = title, name1 = name1,
                         name2 = name2, arg1 = arg1, arg2 = arg2, error = error)
 
 class ThanksHandler(Handler):
-	def get(self):
-		self.response.write("Thanks")
+    def get(self):
+        self.response.write("Thanks")
 
 class PlayHandler(Handler):
-    def get(self, play_id):
-        key = db.Key.from_path('Argument', int(play_id))
+    arg = ''
+    def get(self, arg_id):
+        key = db.Key.from_path('Argument', int(arg_id))
         arg = db.get(key)
         if not arg:
             self.error(404)
             return
-        self.render("play.html", arg = arg)#TODO: Make sure elements are in dot notation
+        self.render("start.html", arg = arg)#TODO: Make sure elements are in dot notation
+    def post(self):
+        #change arg's score and rating.
+        up1 = self.request.get('up1')
+        up2 = self.request.get('up2')
+        star = self.request.get('up2')
+        if up1:
+            arg.score_up1()
+        else:
+            arg.score_up2()
+        if star:
+            arg.ratingsUp()
 
 class StartPlayHandler(Handler):
     def get(self):
         #TODO: Get best question/random, existing 
-        p = "best choice"#TODO: REPLACE WITH GQL
-        self.redirect('blog/%s', str(p.key().id()))
+        p = db.GqlQuery("SELECT * FROM Argument ORDER BY rating DESC")[0]
+        self.redirect('start/%s', str(p.key().id()))
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/newArgument', NewArgHandler),
-    ('/play/', StartPlayHandler),
-    ('/play/([0-9]+)', PlayHandler),
+    ('/start/', StartPlayHandler),
+    ('/start/([0-9]+)', PlaytHandler),
     ('/thanks', ThanksHandler)
 ], debug=True)
