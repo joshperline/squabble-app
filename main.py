5c8cb362e418312e016ba66535be99561a08460f
Josh Perline <joshperline@yahoo.com>
2014-03-03 17:11:38 -0800

    100% Working v1.0

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import re
 import jinja2
 import time
 
-#sends input to certain directory or site
-#unless post is there, its a get method (post doesnt put the query in the url)
-#post = uploading/updating, no max length
-#get = recieving stuff, dont change server
-
 """TODO: Profiles later."""
 
 from google.appengine.ext import db
@@ -31,6 +26,8 @@ class Handler(webapp2.RequestHandler):
 
 class IDList(db.Expando):
     theList = db.ListProperty(int)
+    count = 0
+    created = db.DateTimeProperty(auto_now_add = True)
 
 #"Row" in database table
 class Argument(db.Expando):
@@ -40,45 +37,39 @@ class Argument(db.Expando):
     arg1 = db.TextProperty(required = True)
     sex1 = db.StringProperty(required = True)
 
-
     name2 = db.StringProperty(required = True)
     arg2 = db.TextProperty(required = True)
     sex2 = db.StringProperty(required = True)
 
-
     created = db.DateTimeProperty(auto_now_add = True)
 
     #who's winning
-    score1 = db.IntegerProperty(required = True)
-    score2 = db.IntegerProperty(required = True)
+    score1 = 0
+    score2 = 0
 
-    maleCorrect = db.IntegerProperty(required = True)
-    femaleCorrect = db.IntegerProperty(required = True)
+    maleCorrect = 0
+    femaleCorrect = 0
 
-    rating = db.IntegerProperty(required = True)
+    rating = 0
     #improve to hotness algorithm = db.integer
-
-    "**** CHANGE THESE Methods INTO ACTUAL CODE****"
-    def score_up1(self):
-        score1 += 1
-    def score_up2(self):
-        score2 += 1
-
-    def ratingUp(self):
-        rating += 1
 
 class MainHandler(Handler):
     def get(self):
         self.render("index.html")
 
+class AboutHandler(Handler):
+    def get(self):
+        self.render("about.html")
+
+class ContactHandler(Handler):
+    def get(self):
+        self.render("contact.html")
+
 class NewArgHandler(Handler):
     def get(self):
-        # l = IDList(theList = [])
-        # l.put()
         self.render("argument.html")
 
     def post(self):
-        #TODO: do handling on creating a new argument
         title = self.request.get('title')
         name1 = self.request.get('name1')
         name2 = self.request.get('name2')
@@ -91,18 +82,13 @@ class NewArgHandler(Handler):
                           arg2 = arg2, sex1 = sex1, sex2 = sex2, score1 = 0, score2 = 0,
                           rating = 0, maleCorrect = 0, femaleCorrect = 0)
             a.put()
-            dbList = db.GqlQuery("SELECT * FROM IDList")
-            for l in dbList:
-                idList = l
-                break
+
+            idList = db.GqlQuery("SELECT * FROM IDList").get()
             idList.theList.append((a.key().id()))
             idList.put()
 
-            self.response.write(str(len(idList.theList)))
-
-            # url = 'judge/' + str(a.key().id())
-            # self.redirect(url)
-            #self.redirect(url)#TODO: MAKE thanks.html
+            url = 'judge/' + str(a.key().id())
+            self.redirect(url)
         else:
             error = "Please fill in all required information."
             self.render("argument.html", title = title, name1 = name1,
@@ -112,34 +98,27 @@ class ThanksHandler(Handler):
     def get(self):
         self.response.write("Thanks.")
 
-
 class JudgeHandler(Handler):
     def get(self):
-        #TODO: Get best question/random, existing
-        # args = db.GqlQuery("SELECT * FROM Argument ORDER BY created DESC")#DESC=descending order
-        # for arg in args:
-        #     a = arg
-        #     break
+        idList = db.GqlQuery("SELECT * FROM IDList ORDER BY created DESC").get()
+        args = db.GqlQuery("SELECT * FROM Argument ORDER BY rating DESC").fetch(limit=100)
+        a = args[idList.count]
+        idList.count += 1
+        if idList.count >= len(args):
+            idList.count = 0
+        idList.put()
 
-        dbList = db.GqlQuery("SELECT * FROM IDList")
-        for l in dbList:
-            idList = l
-            break
-        "Gets a random key from all existing arguments"
-        key = idList.theList[random.randint(0, len(idList.theList) - 1)]
-        #url = 'judge/' + str(a.key().id())
-        url = 'judge/' + str(key)
+        url = 'judge/' + str(a.key().id())
         self.redirect(url)
 
 class PlayHandler(Handler):
-
     def get(self, arg_id):
         key = db.Key.from_path('Argument', int(arg_id))
         arg = db.get(key)
         if not arg:
             self.error(404)
             return
-        self.render("judge.html", squabble = arg)#TODO: Make sure elements are in dot notation
+        self.render("judge.html", squabble = arg)
 
     def post(self, arg_id):
         #change arg's score and rating.
@@ -148,29 +127,20 @@ class PlayHandler(Handler):
         decision = self.request.get('decision')
         star = self.request.get('favorite')
         if (decision):
-            #Also keeps track of male vs female
             if decision == "1":
-                #arg.score_up1()
                 arg.score1 += 1
                 if arg.sex1 == "male":
-                    #arg.maleCorrect()
                     arg.maleCorrect += 1
                 else:
-                    #arg.femaleCorrect()
                     arg.femaleCorrect += 1
             elif decision == "2":
-                #arg.score_up2()
                 arg.score2 += 1
                 if arg.sex2 == "male":
-                    #arg.maleCorrect()
                     arg.maleCorrect += 1
                 else:
-                    #arg.femaleCorrect()
                     arg.femaleCorrect += 1
             if star:
-                #arg.ratingsUp()
                 arg.rating += 1
-            #TODO: DELETE AND FIX
             arg.put()
             self.redirect("/judge")
         else:
@@ -182,5 +152,7 @@ app = webapp2.WSGIApplication([
     ('/argument', NewArgHandler),
     ('/judge', JudgeHandler),
     ('/judge/([0-9]+)', PlayHandler),
+    ('/about', AboutHandler),
+    ('/contact', ContactHandler),
     ('/thanks', ThanksHandler)
 ], debug=True)
